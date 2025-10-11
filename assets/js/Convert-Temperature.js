bb61b25a6d6d4e845a7c689adf9369e4886828a7
Jonathan Davies <jonathan.davies92@gmail.com>
2025-10-11 18:12:10 +0100

    Base files for temperature conversion

---
+++ assets/js/Convert-Temperature.js
@@ -0,0 +1,15 @@
+/**
+ * @author jonathan-davies-uk
+ * @copyright Jonathan Davies 2025
+ * @license Apache-2.0
+ */
+
+/** Convert Temperature operation */
+
+const EMPERATURE_UNITS = [
+    "[Temperature]", "Celsius (°C)", "Fahrenheit (°F)", "Kelvin (K)",
+];
+
+const TEMPERATURE_MULTIPLES = { //Multiples of Celsius
+
+};
